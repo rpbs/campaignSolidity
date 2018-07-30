6cc73c5f1c22c70f87d06523700cfe65a9e636b4
rpbs <renyopaulo@gmail.com>
2018-07-30 19:35:27 -0300

    compilados

---
+++ etherium/compile.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs-extra');
+const solc = require('solc');
+
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+const campanhaPath = path.resolve(__dirname, 'contracts', 'Campanha.sol');
+const source = fs.readFileSync(campanhaPath, 'utf8');
+const output = solc.compile(source, 1).contracts;
+
+fs.ensureDirSync(buildPath);
+
+for (let c in output){
+    fs.outputJsonSync(
+        path.resolve(buildPath, c.replace(':','') + '.json'),
+        output[c]
+    );
+}
