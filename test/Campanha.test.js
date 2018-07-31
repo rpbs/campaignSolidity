7c47ca3c8d8b238b5461fd3ae11563cd229bbd13
rpbs <renyopaulo@gmail.com>
2018-07-30 21:01:26 -0300

    testes...mais...

--- test/Campanha.test.js
+++ test/Campanha.test.js
@@ -5,25 +5,26 @@ const web3 = new Web3(ganache.provider());
 
 const compiledFactory = require('../etherium/build/CampanhaFactory.json');
 
-//const campanha = require('../etherium/build/Campanha.json');
+let endereco;
+const campanha = require('../etherium/build/Campanha.json');
 
-let accounts, factory, campanhaAddress, campanha;
+let accounts, factory, campanhaAddress, campanhaContrato;
 
 beforeEach(async() => {
   accounts = await web3.eth.getAccounts();
-
+  endereco = accounts[0];
   factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
     .deploy({ data: compiledFactory.bytecode })
-    .send({ from: accounts[0], gas: '1000000' });
+    .send({ from: endereco, gas: '1000000' });
 
-    await factory.methods.criarCampanha(100).send({
-    from: accounts[0],
+  await factory.methods.criarCampanha('100').send({
+    from: endereco,
     gas: '1000000'
   });
 
   [campanhaAddress] = await factory.methods.getDeployed().call();
-  campanha = await new web3.eth.Contract(
-    JSON.parse(compiledFactory.interface),
+  campanhaContrato = await new web3.eth.Contract(
+    JSON.parse(campanha.interface),
     campanhaAddress
   );
 
@@ -32,6 +33,21 @@ beforeEach(async() => {
 describe('Campanhas', () => {
   it('factory e campanha', () => {
     assert.ok(factory.options.address);
-    assert.ok(campanha.options.address);
+    assert.ok(campanhaContrato.options.address);
+  });
+
+  it('quem criou o contrato é o dono dele...', async () => {
+    const manager = await campanhaContrato.methods.manager().call();
+    assert.equal(endereco, manager);
+  });
+
+  it('pessoas doando e sendo aprovadores...', async () => {
+    await campanhaContrato.methods.contribute().send({
+      value: '200',
+      from: accounts[1]
+    });
+
+    let contribuiu = await campanhaContrato.methods.aprovadores(accounts[1]).call();
+    assert(contribuiu);
   });
 });
