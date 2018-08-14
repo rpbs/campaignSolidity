8252d9224caa6074aa0fcf90a7292ede975f4d17
rpbs <renyopaulo@gmail.com>
2018-08-13 23:04:41 -0300

    deixando a página mais bonita

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import factory from '../etherium/factory';
+import { Card } from 'semantic-ui-react';
 
 class CampaignIndex extends Component {
+
   // criar instancias de método na própria classe...
   //... isso é exigido pelo next....
   // é exigido porque neste ponto estamos no NextServer
@@ -14,8 +16,26 @@ class CampaignIndex extends Component {
     return { campanhas };
   }
 
+  renderCampanhas(){
+    const items = this.props.campanhas.map(address =>{
+      return {
+        header:address,
+        description: <a>Ver campanha</a>,
+        meta: "asd",
+        fluid: true,
+      }
+    });
+
+
+
+    return <Card.Group items={items} />;
+  }
+
   render(){
-    return <div>{this.props.campanhas}</div>
+    return <div>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"></link>
+    {this.renderCampanhas()}
+    </div>
   }
 }
 
