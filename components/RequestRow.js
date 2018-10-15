cfe26d08479331f5b45c8bba3f0d682dcf7b8c36
rpbs <renyopaulo@gmail.com>
2018-10-15 20:17:42 -0300

    ddd

---
+++ components/RequestRow.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Form, Input, Message, Button } from 'semantic-ui-react';
+import Campaign from '../etherium/campaign';
+import web3 from '../etherium/web3';
+import { Router } from '../routes'; // { com chaves porque exporta muitas coisas}
+
+class RequestRow extends Component {
+  render(){
+    return "asd";
+  };
+}
+
+export default RequestRow;
